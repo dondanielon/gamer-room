5f8ddb950d599ccacf22438a8480e9a5de0e95bf
Daniel Valencia <becse.ca@gmail.com>
2023-03-17 21:40:21 -0600

    general layout implementation

---
+++ client/src/components/Layout.tsx
@@ -0,0 +1,20 @@
+import style from '@/components/styles/Layout.module.scss'
+import { LayoutProps } from '@/types/components'
+
+export default function Layout(props: LayoutProps) {
+    return (
+        <main 
+            className={style.main}
+            style={
+                {
+                    justifyContent: props.horizontalCenter ? 'center' : 'normal',
+                    alignItems: props.verticalCenter ? 'center' : 'normal'
+                }
+            }
+        >
+            <section>
+                {props.children}
+            </section>
+        </main>
+    )
+}
