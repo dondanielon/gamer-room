fb8d8c5e3de5cbb0b5aec21d4359bc62df4dd864
Daniel Valencia <becse.ca@gmail.com>
2023-03-19 19:44:22 -0600

    Loading component

---
+++ client/src/components/Loading.tsx
@@ -0,0 +1,18 @@
+import Layout from "./Layout";
+import { ColorRing } from  'react-loader-spinner'
+
+export default function Loading() {
+    return (
+        <Layout verticalCenter={true} horizontalCenter={true}>
+            <ColorRing
+                visible={true}
+                height="80"
+                width="80"
+                ariaLabel="blocks-loading"
+                wrapperStyle={{}}
+                wrapperClass="blocks-wrapper"
+                colors={['#a9a9c3', '#a9a9c3', '#a9a9c3', '#a9a9c3', '#a9a9c3']}
+            />
+        </Layout>
+    )
+}
--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -1,19 +1,15 @@
 import Layout from "@/components/Layout";
+import Loading from "@/components/Loading";
 import useProtect from "@/hooks/useProtect";
-
 
 export default function Dashboard() {
     const { isPageLoading } = useProtect()
    
     return isPageLoading 
         ? (
-            <Layout horizontalCenter={true} verticalCenter={true}>
-                <div>
-                     Loading
-                </div>
-            </Layout>
+            <Loading />
         ) : (
-            <Layout horizontalCenter={true} verticalCenter={false}>
+            <Layout horizontalCenter={true} verticalCenter={true}>
                 <div>
                      Dashboard
                 </div>
