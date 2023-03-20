1c027f4ca3d4f425d5d56b729e65e7ae2dff3232
Daniel Valencia <becse.ca@gmail.com>
2023-03-19 19:42:57 -0600

    custom hook for protected pages

--- client/src/hooks/useProtect.ts
+++ client/src/hooks/useProtect.ts
@@ -1,19 +1,15 @@
-import { setRedirectAfterSignIn } from "@/redux/reducers/auth"
-import { RootState } from "@/redux/store"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
+import useAuth from "./useAuth"
 
 export default function useProtect() {
-    const isUserLoggedIn = useSelector(({ auth }: RootState) => auth.isUserLoggedIn )
-    const accessToken = useSelector(({ auth }: RootState) => auth.accessToken )
+    const {isUserLoggedIn, accessToken, setRedirectPath} = useAuth()
     const [isPageLoading, setIsPageLoading] = useState(true)
     const router = useRouter()
-    const dispatch = useDispatch()
 
     useEffect(() => {
         if (!isUserLoggedIn || !accessToken) {
-            dispatch(setRedirectAfterSignIn(router.pathname))
+            setRedirectPath(router.pathname)
             router.push('/login')
         }
 
