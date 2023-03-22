b84e16bdc21e902cbf3d4eaa6f33b60b6dc79089
Daniel Valencia <becse.ca@gmail.com>
2023-03-22 00:31:44 -0600

    dashboard layout and styles

---
+++ client/src/components/DashboardLayout.tsx
@@ -0,0 +1,22 @@
+import style from '@/components/styles/DashboardLayout.module.scss'
+import { LayoutProps } from '@/types/components'
+import DashboardMenu from './DashboardMenu'
+
+export default function DashboardLayout(props: LayoutProps) {
+    return (
+        <main 
+            className={style.main}
+            style={
+                {
+                    justifyContent: props.horizontalCenter ? 'center' : 'normal',
+                    alignItems: props.verticalCenter ? 'center' : 'normal'
+                }
+            }
+        >
+            <DashboardMenu />
+            <section>
+                {props.children}
+            </section>
+        </main>
+    )
+}
