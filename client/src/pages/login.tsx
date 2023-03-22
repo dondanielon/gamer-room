01f422bd831dcd955b581cc9642450716b065499
Daniel Valencia <becse.ca@gmail.com>
2023-03-21 20:55:10 -0600

    condition see if user is logged in to redirect

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,9 +1,27 @@
 import Layout from "@/components/Layout" 
+import Loading from "@/components/Loading"
 import LoginForm from "@/components/LoginForm"
+import useAuth from "@/hooks/useAuth"
 import style from '@/pages/styles/login.module.scss'
+import { useRouter } from "next/router"
+import { useState, useEffect } from "react"
 
 export default function Login() {
-    return (
+    const router = useRouter()
+    const { isUserLoggedIn, accessToken, credentials } = useAuth()
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        if (isUserLoggedIn && accessToken && credentials) {
+            router.push('/dashboard')
+        } else {
+            setIsLoading(false)
+        }
+    }, [])
+
+    return isLoading ? (
+        <Loading />
+    ) : (
         <Layout horizontalCenter={true} verticalCenter={true}>
             <div className={style.container}>
                 <LoginForm />
