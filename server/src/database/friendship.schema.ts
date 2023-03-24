9627e5b4681d38f78e026e4bf70ecf8965446816
Daniel Valencia <becse.ca@gmail.com>
2023-03-24 00:14:14 -0600

    small changes to friendship schema

--- server/src/database/friendship.schema.ts
+++ server/src/database/friendship.schema.ts
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema
 const Friendship = mongoose.model(
     "friendship",
     new Schema({
-        sender: { type: Schema.Types.ObjectId, required: true },
-        receiver: { type: Schema.Types.ObjectId, required: true },
-        status: { type: String, required: true, enum: ["pending" || "accepted" || "declined"] },
+        sender: { type: Schema.Types.ObjectId, required: true, index: true },
+        receiver: { type: Schema.Types.ObjectId, required: true, index: true },
+        status: { type: String, required: true, enum: ["pending" || "accepted" || "declined"], default: "pending" },
         createdAt: { type: Date, required: true },
         acceptedAt: { type: Date }
     })
