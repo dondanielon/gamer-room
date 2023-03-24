17bd0757b5570afe4b55dd0b98f37d3f7caafe36
Daniel Valencia <becse.ca@gmail.com>
2023-03-24 00:14:36 -0600

    new helpers

--- server/src/database/helpers.ts
+++ server/src/database/helpers.ts
@@ -1,5 +1,6 @@
 import User from "./user.schema";
 import { ISignup } from "../types/handlers";
+import Friendship from "./friendship.schema";
 
 export async function createUser(user: ISignup) {
     try {
@@ -23,11 +24,33 @@ export async function findUserByEmail(email: string) {
     }
 }
 
+export async function findUserById(id: string) {
+    try {
+        const user = await User.findById(id)
+        return user;
+    } catch (error) {
+        throw error;
+    }
+}
+
 export async function findUserWithRefreshToken(refreshToken: string) {
     try {
         const user = await User.findOne({ refreshToken: refreshToken });
         return user;
     } catch (error) {
         throw error;
+    }
+}
+
+export async function createFriendship(sender: string, receiver: string) {
+    try {
+        const friendRequest = new Friendship({
+            sender: sender,
+            receiver: receiver,
+            createdAt: new Date()
+        })
+        await friendRequest.save()
+    } catch (error) {
+       throw error 
     }
 }
