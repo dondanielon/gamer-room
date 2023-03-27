d1838f5009db6a9b5c71b9316f9c70d72b771ae1
Daniel Valencia <becse.ca@gmail.com>
2023-03-26 22:55:53 -0600

    socket dependency installed

--- client/src/components/AuthProvider.tsx
+++ client/src/components/AuthProvider.tsx
@@ -1,27 +1,27 @@
-import useAuth from "@/hooks/useAuth";
-import { useEffect, useState } from "react";
-import Loading from "./Loading";
-import { ChildrenProps } from "@/types/components";
+import useAuth from "@/hooks/useAuth"
+import { useEffect, useState } from "react"
+import Loading from "./Loading"
+import { ChildrenProps } from "@/types/components"
 
 export default function AuthProvider(props: ChildrenProps) {
-    const { accessToken, refreshToken, isUserLoggedIn, credentials } = useAuth();
-    const [isLoading, setIsLoading] = useState(true);
+    const { accessToken, refreshToken, isUserLoggedIn, credentials } = useAuth()
+    const [isLoading, setIsLoading] = useState(true)
 
     const verifyRefreshToken = async () => {
         try {
-            await refreshToken();
+            await refreshToken()
         } catch (error) {
-            console.error(error);
+            console.error(error)
         } finally {
-            setIsLoading(false);
+            setIsLoading(false)
         }
     };
 
     useEffect(() => {
         !accessToken || !isUserLoggedIn || !credentials
             ? verifyRefreshToken()
-            : setIsLoading(false);
+            : setIsLoading(false)
     }, []);
 
-    return <>{isLoading ? <Loading /> : props.children}</>;
+    return <>{isLoading ? <Loading /> : props.children}</>
 }
