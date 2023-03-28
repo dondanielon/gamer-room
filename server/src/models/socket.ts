f63d2cbb028ee8cf2dc31a7920e42b15409093d7
Daniel Valencia <becse.ca@gmail.com>
2023-03-27 20:58:04 -0600

    minor change to server

--- server/src/models/socket.ts
+++ server/src/models/socket.ts
@@ -1,26 +1,33 @@
-import { Server, Socket } from "socket.io"
+import { Server } from "socket.io"
 import http from "http"
 
 class SocketConfiguration {
     private io: Server
-    private connections: any
+    private connections: { [socketId: string]: string }
 
     constructor(server: http.Server, clientURL: string) {
         this.io = new Server(server, { cors: { origin: clientURL } })
-        console.log(clientURL)
         this.connections = {}
-
         this.main()
     }
 
     private main() {
         this.io.on('connection', (socket) => {
-            socket.emit("connection")
+            console.log(`user connected: ${socket.id}`)
+            this.io.emit("connection")
             socket.on("set-credentials", (credentials: string) => {
                 this.connections["$." + socket.id] = credentials 
+
+                socket.on("get-count", () => {
+                    console.log(`sending count`)
+                    socket.emit("send-count", this.connections)
+                    console.log(this.connections)
+                })
             })
 
-            this.rooms(socket)
+            
+
+            //SocketConfiguration.rooms(socket)
 
             socket.on('disconnect', () => {
                 delete this.connections["$." + socket.id]
@@ -28,9 +35,11 @@ class SocketConfiguration {
         })
     }
 
-    private rooms(socket: Socket) {
-        socket.on("rooms", () => {})
-    }
+    // private static rooms(_socket: Socket) {
+    //     // socket.on("get-count", () => {
+    //     //     socket.emit("send-count", connections)
+    //     // })
+    // }
 }
 
 export default SocketConfiguration
