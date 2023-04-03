6401d7dbaacf5478b5831762610dac222ab101f6
Daniel Valencia <becse.ca@gmail.com>
2023-04-02 19:23:13 -0600

    set standings and offense stats functions

--- server/src/models/nflScraper.ts
+++ server/src/models/nflScraper.ts
@@ -1,106 +1,25 @@
 import axios, { AxiosInstance } from "axios"
 import * as cheerio from "cheerio"
+import { NFLTeamI, SetTeamsOffenseI, SetTeamsStandingsI } from "../types/scrapers"
 
-interface Team {
-    name: string
-        code: string
-        standings: {
-            wins: number
-            losses: number
-            ties: number
-            winPercentage: number
-            localRecord: string
-            visitorRecord: string
-            divisionRecord: string
-            conferenceRecord: string
-            pointsFor: number
-            pointsAgainst: number
-            pointsDifferential: number
-            currentStreak: string
-        },
-        stats: {
-            offense: {
-                totalYards: number
-                yardsPerGame: number
-                passing: {
-                    total: number
-                    perGame: number
-                },
-                rushing: {
-                    total: number
-                    perGame: number
-                },
-                points: {
-                    total: number
-                    perGame: number
-                }
-            }
-            defense: {
-                totalYards: number
-                yardsPerGame: number
-                passing: {
-                    total: number
-                    perGame: number
-                },
-                rushing: {
-                    total: number
-                    perGame: number
-                },
-                points: {
-                    total: number
-                    perGame: number
-                }
-            }
-            specialTeams: {
-                kickoffs: {
-                    kickAttempts: number
-                    totalYards: number
-                    averageYardsPerKick: number
-                    long: number
-                    touchdowns: number
-                }
-                punts: {
-                    puntAttempts: number
-                    totalYards: number
-                    averageYardsPerPunt: number
-                    long: number
-                    touchdowns: number
-                    fairCatches: number
-                }
-            }
-            turnovers: {
-                ratio: number
-                takeaways: {
-                    interceptions: number
-                    fumbles: number
-                    total: number
-                }
-                giveaways: {
-                    interceptions: number
-                    fumbles: number
-                    total: number
-                }
-            }
-        }
-}
-interface Test {
-    [key: string]: Team
-}
+// const teamsInLeague = [
+
+// ]
 
 class NFLScraper {
     private TEAM_STANDINGS_URL: string
-    // private TEAM_OFFENSE_STATS_URL: string
-    // private TEAM_DEFENSE_STATS_URL: string
+    private TEAM_OFFENSE_STATS_URL: string
+    private TEAM_DEFENSE_STATS_URL: string
     // private TEAM_SPECIAL_TEAMS_STATS_URL: string
     // private TEAM_TURNOVERS_STATS_URL: string
     private axiosInstance: AxiosInstance
-    private teams: Array<Team>
+    private teams: Array<NFLTeamI>
 
 
     constructor() {
         this.TEAM_STANDINGS_URL = "https://www.espn.com/nfl/standings/_/group/league"
-        // this.TEAM_OFFENSE_STATS_URL = "https://www.espn.com/nfl/stats/team/_/view/offense"
-        // this.TEAM_DEFENSE_STATS_URL = "https://www.espn.com/nfl/stats/team/_/view/defense"
+        this.TEAM_OFFENSE_STATS_URL = "https://www.espn.com/nfl/stats/team/_/view/offense"
+        this.TEAM_DEFENSE_STATS_URL = "https://www.espn.com/nfl/stats/team/_/view/defense"
         // this.TEAM_SPECIAL_TEAMS_STATS_URL = "https://www.espn.com/nfl/stats/team/_/view/special"
         // this.TEAM_TURNOVERS_STATS_URL = "https://www.espn.com/nfl/stats/team/_/view/turnovers"
         this.axiosInstance = axios.create({
@@ -109,86 +28,190 @@ class NFLScraper {
         this.teams = []
     }
 
-    async launch() {
+    getTeam(teamCode: string) {
+        const team = this.teams.find(team => teamCode === team.code)
+        return team ? team : "INVALID TEAM"
+    }
+
+    async scrap() {
         try {
-            await this.setTeamsAndStandings()
-            await this.setTeamOffenseStats()
-            console.log(this.teams)
+            const start = Date.now()
+            // const teamStandings = await this.setTeamsStandings()
+            // const teamsOffense = await this.setTeamOffenseStats()
+            // if (teamStandings && teamsOffense) console.log(teamStandings)
+            
+            const [standings, offense] = await Promise.all([
+                this.setTeamsStandings(),
+                this.setTeamsOffenseStats()
+            ])
+            const end = Date.now()
+            console.log(`Scraping took ${end - start}ms`)
         } catch (error) {
             console.log(error)
             throw error
         }
     }
+    
+    // private async setTeamsDefenseStats() {
+    //     const axiosResponse = await this.axiosInstance.request({
+    //         method: "GET",
+    //         url: this.TEAM_DEFENSE_STATS_URL,
+    //     })
+    //     const data: SetTeamsOffenseI = {}
+    //     const $ = cheerio.load(axiosResponse.data)
 
-    private async setTeamOffenseStats() {
-        // const axiosResponse = await this.axiosInstance.request({
-        //     method: "GET",
-        //     url: this.TEAM_OFFENSE_STATS_URL,
-        // })
+    //     $("[data-idx]").each((_index, element) => {
+    //         const htmlIndex = element.attribs["data-idx"]
+    //         const statsArr: Array<string> = []
+    //         const teamName = $(element).find(".Image").attr("title")
+
+    //         $(element).find(".Table__TD").each((_idx, elm) => {
+    //             let stat = $(elm).find("div").text()
+    //             stat = stat.includes(",") ? stat.replace(",", "") : stat
+    //             statsArr.push(stat)
+    //         })
+    //         statsArr.shift()
+            
+    //         if (teamName) {
+    //             data[htmlIndex] = {
+    //                 ...data[htmlIndex],
+    //                 name: teamName
+    //             }
+    //         }
+
+    //         if (statsArr.length === 8) {
+    //             const offenseProperties = [
+    //                 "totalYards", "yardsPerGame", "totalPassingYards", "passingYardsPerGame",
+    //                 "totalRushingYards", "rushingYardsPerGame", "totalPoints" ,"pointsPerGame"
+    //             ]
+
+    //             offenseProperties.forEach((item, index) => {
+    //                 data[htmlIndex] = {
+    //                     ...data[htmlIndex],
+    //                     offense: {
+    //                         ...data[htmlIndex].offense,
+    //                         [item]: parseFloat(statsArr[index]) 
+    //                     }
+    //                 }
+    //             })
+    //         }
+    //     })
+    //     return Object.values(data)
+    // }
+
+    private async setTeamsOffenseStats() {
+        const axiosResponse = await this.axiosInstance.request({
+            method: "GET",
+            url: this.TEAM_OFFENSE_STATS_URL,
+        })
+        const data: SetTeamsOffenseI = {}
+        const $ = cheerio.load(axiosResponse.data)
+
+        $("[data-idx]").each((_index, element) => {
+            const htmlIndex = element.attribs["data-idx"]
+            const statsArr: Array<string> = []
+            const teamName = $(element).find(".Image").attr("title")
+
+            $(element).find(".Table__TD").each((_idx, elm) => {
+                let stat = $(elm).find("div").text()
+                stat = stat.includes(",") ? stat.replace(",", "") : stat
+                statsArr.push(stat)
+            })
+            statsArr.shift()
+            
+            if (teamName) {
+                data[htmlIndex] = {
+                    ...data[htmlIndex],
+                    name: teamName
+                }
+            }
+
+            if (statsArr.length === 8) {
+                const offenseProperties = [
+                    "totalYards", "yardsPerGame", "totalPassingYards", "passingYardsPerGame",
+                    "totalRushingYards", "rushingYardsPerGame", "totalPoints" ,"pointsPerGame"
+                ]
+
+                offenseProperties.forEach((item, index) => {
+                    data[htmlIndex] = {
+                        ...data[htmlIndex],
+                        offense: {
+                            ...data[htmlIndex].offense,
+                            [item]: parseFloat(statsArr[index]) 
+                        }
+                    }
+                })
+            }
+        })
+        return Object.values(data)
     }
 
-    private async setTeamsAndStandings() {
+    private async setTeamsStandings() {
         try {
             const axiosResponse = await this.axiosInstance.request({
                 method: "GET",
                 url: this.TEAM_STANDINGS_URL
             })
-            const dataTransformed: Test = {}
-
+            const data: SetTeamsStandingsI = {}
             const $ = cheerio.load(axiosResponse.data)
-            $("[data-idx]").each((_index, element) => {
 
+            $("[data-idx]").each((_index, element) => {
                 const teamName = $(element).find(".hide-mobile").find(".AnchorLink").text()
                 const teamCode = $(element).find(".show-mobile").find("abbr").text()
-                const stats = $(element).find("span.stat-cell")
+                const standings = $(element).find("span.stat-cell")
+                const htmlIndex = element.attribs["data-idx"]
 
                 if (teamName && teamCode) {
-                    dataTransformed[element.attribs["data-idx"]] = {
-                        ...dataTransformed[element.attribs["data-idx"]],
+                    data[htmlIndex] = {
+                        ...data[htmlIndex],
                         name: teamName,
                         code: teamCode
                     }
                 }
 
-                if (stats) {
-                    let props: any = {}
-                    const statsProperties = [
+                if (standings) {
+                    const standingsProperties = [
                         "wins", "losses", "ties", "winPercentage",
                         "homeRecord", "awayRecord", "divisionRecord", "conferenceRecord",
                         "pointsFor", "pointsAgainst", "pointsDifferential", "currentStreak"
                     ]
+                    standings.each((idx, elm) => {
+                        let value: string | number
 
-                    stats.each((idx, elm) => {
-                        props = {
-                            ...props,
-                            [statsProperties[idx]]: $(elm).text()
+                        if (
+                            standingsProperties[idx] === "homeRecord" ||
+                            standingsProperties[idx] === "awayRecord" ||
+                            standingsProperties[idx] === "divisionRecord" ||
+                            standingsProperties[idx] === "conferenceRecord" ||
+                            standingsProperties[idx] === "currentStreak"
+                        ) {
+                            value = $(elm).text()
+                        } else {
+                            value = parseFloat($(elm).text())
+                        }
+                        data[htmlIndex] = {
+                            ...data[htmlIndex],
+                            standings: {
+                                ...data[htmlIndex].standings,
+                                [standingsProperties[idx]]: value
+                            }
                         }
                     })
-
-                    dataTransformed[element.attribs["data-idx"]] = {
-                        ...dataTransformed[element.attribs["data-idx"]],
-                        standings: {
-                            ...props, 
-                            wins: parseInt(props.wins),
-                            losses: parseInt(props.losses),
-                            ties: parseInt(props.ties),
-                            winPercentage: parseFloat(props.winPercentage),
-                            pointsFor: parseInt(props.pointsFor),
-                            pointsAgainst: parseInt(props.pointsAgainst),
-                            pointsDifferential: parseInt(props.pointsDifferential)
-                        }
-                    }
                 }
             })
-
-            for (const [_key, value] of Object.entries(dataTransformed)) {
-                this.teams.push(value)
-            }
+            return Object.values(data)
         } catch (error) {
             throw error
         }
     }
 }
 
-const nfl = new NFLScraper()
-nfl.launch()
+async function start() {
+    const nfl = new NFLScraper()
+    await nfl.scrap()  
+}
+
+start()
+
+
+
